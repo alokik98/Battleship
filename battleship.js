d1ea9a8434455b6d6aaee51485bf32ba3cad9195
alokik98 <alorshsaxena12@gmail.com>
2022-03-12 18:20:19 +0530

    Completing the game functionality

--- battleship.js
+++ battleship.js
@@ -81,3 +81,27 @@ function parseGuess(guess) {
   }
   return null;
 }
+
+function init() {
+  var fireButton = document.getElementById("fireButton");
+  fireButton.onclick = handleFireButton;
+  var guessInput = document.getElementById("guessInput");
+  guessInput.onkeydown = handleKeyPress;
+}
+
+function handleFireButton() {
+  var guessInput = document.getElementById("guessInput");
+  var guess = guessInput.value;
+  controller.processGuess(guess);
+  guessInput.value = "";
+}
+
+function handleKeyPress(e) {
+  var fireButton = document.getElementById("fireButton");
+  if(e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
+}
+
+window.onload = init;
